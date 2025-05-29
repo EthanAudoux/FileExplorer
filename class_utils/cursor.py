3feca6c16edf127797f3dd0271ff7bde1c3a230b
EthanAudoux <ethan@audoux.net>
2025-05-29 18:57:33 +0200

    Enhance Cursor and Node classes: add detailed docstrings for methods to improve code documentation and clarity

--- class_utils/cursor.py
+++ class_utils/cursor.py
@@ -3,8 +3,28 @@ from .node import Node
 from .enumeration import SortFile
 
 class Cursor:
+    """
+    Class to manage the cursor in the file system.
+    It allows navigation through directories, selection of files,
+    copying, cutting, pasting, deleting files, and managing file permissions.
+    It also provides methods to create files and directories, rename files,
+    and sort files based on different criteria.
+    Attributes:
+        node (Node): The current node the cursor is pointing to.
+        current (int): The index of the currently selected item in the node's content.
+        hiddens (bool): Flag to indicate whether hidden files should be shown.
+        fileToCopy (Node): The file or directory to be copied or cut.
+        modeCopy (bool): Flag to indicate whether the operation is a copy (True) or cut (False).
+        sortMode (SortFile): The current sorting mode for files (by name, date, or size).
+    """
     
     def __init__(self, node : Node) -> None:
+        """
+        Initialize the Cursor with a given node.
+        Parameters:
+            node (Node): The Node object representing the current directory or file.
+        """
+        
         self.node : Node = node
         self.current = 0
                 
@@ -17,10 +37,21 @@ class Cursor:
          
 
     def get_node(self) -> Node:
+        """
+        Get the current node the cursor is pointing to.
+        Returns:
+            Node: The Node object representing the current directory or file.
+        """
         return self.node
     
 
     def cursor_down(self, modeNew : bool = False):
+        """
+        Move the cursor down to the next item in the current node's content.
+        If modeNew is True, it will always move down regardless of the current index.
+        Parameters:
+            modeNew (bool): If True, move down without checking the current index.
+        """
         if modeNew:
             self.current += 1
         else:
@@ -33,6 +64,12 @@ class Cursor:
 
 
     def cursor_up(self, modeNew : bool = False):
+        """
+        Move the cursor up to the previous item in the current node's content.
+        If modeNew is True, it will always move up regardless of the current index.
+        Parameters:
+            modeNew (bool): If True, move up without checking the current index.
+        """
         if modeNew:
             self.current -= 1
         else:
@@ -45,11 +82,23 @@ class Cursor:
         
 
     def select(self):
+        """
+        Select the current item in the node's content.
+        """
         self.node = self.get_current_selected()
         self.current = 0
     
 
     def get_content(self, filter : str = '') -> list:
+        """
+        Get the content of the current node.
+        If the node is a directory, it returns its children sorted by the current sort mode.
+        If the node is a file, it returns the content of the file.
+        Parameters:
+            filter (str): A string to filter the content by name.
+        Returns:
+            list: A list of objects representing the content of the node. Strings for files, Node objects for directories.
+        """
         res = []
         if self.node.is_dir():
             res = self.node.get_children(self.hiddens)
@@ -65,10 +114,19 @@ class Cursor:
     
 
     def get_current_index(self) -> int:
+        """
+        Get the index of the currently selected item in the node's content.
+        Returns:
+            int: The index of the currently selected item.
+        """
         return self.current
     
 
     def get_parent(self):
+        """
+        Move the cursor to the parent directory of the current node.
+        If the current node is the root directory, it does nothing.
+        """
         previous = self.node.get_name()
         self.node = self.node.get_parent()
         self.current = next((i for i, obj in enumerate(self.get_content()) if obj.get_name() == previous), 0)
@@ -76,51 +134,115 @@ class Cursor:
 
 
     def get_toggle_hidden(self):
+        """
+        Toggle the visibility of hidden files in the current node's content.
+        If hidden files are currently shown, it hides them, and vice versa.
+        """
         self.hiddens = not self.hiddens
         self.current = 0
 
 
     def get_current_selected(self) -> Node:
+        """
+        Get the currently selected item in the node's content.
+        Returns:
+            Node: The currently selected Node object.
+        """
         return self.get_content()[self.current]
     
     
     def copySet(self):
+        """
+        Set the file or directory to be copied.
+        It stores the currently selected item in the fileToCopy attribute
+        and sets the modeCopy flag to True.
+        """
         self.fileToCopy = self.get_current_selected()
         self.modeCopy = True
     
     def cutSet(self):
+        """
+        Set the file or directory to be cut.
+        It stores the currently selected item in the fileToCopy attribute
+        and sets the modeCopy flag to False.
+        """
         self.fileToCopy = self.get_current_selected()
         self.modeCopy = False
         
     def paste(self):
+        """
+        Paste the copied or cut file or directory to the current node.
+        If modeCopy is True, it copies the file; otherwise, it moves the file.
+        It uses the copy or move method of the Node class.
+        """
         if self.modeCopy:
             self.fileToCopy.copy(self.node)
         else:
             self.fileToCopy.move(self.node)
         
     def delete(self):
+        """
+        Delete the currently selected file or directory.
+        It calls the delete method of the Node class for the currently selected item.
+        If the current index is out of bounds after deletion, it adjusts the current index.
+        """
         self.get_current_selected().delete()
         
         if self.current > len(self.get_content()) - 1:
             self.current = len(self.get_content()) - 1
             
-    def get_permissions(self):
+    def get_permissions(self)-> str:
+        """
+        Get the permissions of the currently selected file or directory.
+        It calls the get_permissions method of the Node class for the currently selected item.
+        Returns:
+            str: A string representing the permissions of the currently selected item.
+        """
         return self.get_current_selected().get_permissions()
     
     def create_file(self, name):
+        """
+        Create a new file with the specified name in the current node.
+        If the name is not an empty string, it calls the create_file method of the Node class.
+        Parameters:
+            name (str): The name of the file to be created.
+        """
         if name != "":
             self.node.create_file(name)
     
     def create_dir(self, name):
+        """
+        Create a new directory with the specified name in the current node.
+        If the name is not an empty string, it calls the create_dir method of the Node class.
+        Parameters:
+            name (str): The name of the directory to be created.
+        """
         if name != "":
             self.node.create_dir(name)
         
     def rename(self, name):
+        """
+        Rename the currently selected file or directory to the specified name.
+        If the name is not an empty string, it calls the rename method of the Node class.
+        Parameters:
+            name (str): The new name for the file or directory.
+        """
         if name != "":
             self.get_current_selected().rename(name)
         
         
     def sortNode(self, child):
+        """
+        Sort the child node based on the current sort mode.
+        It returns a value based on the sort mode:
+        - If sort mode is NAME, it returns the name of the child in lowercase.
+        - If sort mode is DATE, it returns the date of the child.
+        - If sort mode is SIZE, it returns the size of the child.
+        Parameters:
+            child (Node): The child node to be sorted.
+        Returns:
+            sstr or int: A value used for sorting the child node.
+        """
         if self.sortMode == SortFile.NAME:
             return child.get_name().lower()
         elif self.sortMode == SortFile.DATE:
@@ -129,6 +251,10 @@ class Cursor:
             return child.get_size()
     
     def changeSort(self):
+        """
+        Change the current sort mode of the cursor.
+        It cycles through the sort modes: NAME -> DATE -> SIZE -> NAME.
+        """
         if self.sortMode == SortFile.NAME:
             self.sortMode = SortFile.DATE
         elif self.sortMode == SortFile.DATE:
--- class_utils/node.py
+++ class_utils/node.py
@@ -7,43 +7,50 @@ class Node:
     Class representing a file or directory in the filesystem.
     Provides methods to interact with the filesystem, such as getting children,
     copying, moving, deleting, and retrieving metadata.
+    Attributes:
+        path (str): The absolute path to the file or directory.
     """
     
     def __init__(self, path : str):
         """
         Initialize the Node with a given path.
-        @param path: The path to the file or directory.
+        Parameters:
+            path (str): The path to the file or directory.
         """
         self.path = os.path.abspath(path)
     
-    def get_path(self):
+    def get_path(self)-> str:
         """
         Get the absolute path of the node.
-        @return: The absolute path as a string.
+        Returns:
+            str: The absolute path as a string.
         """
         return os.path.abspath(self.path)
     
-    def get_name(self):
+    def get_name(self)-> str:
         """
         Get the name of the node (file or directory).
-        @return: The name of the node as a string.
+        Returns:
+            str: The name of the node as a string.
         """
         if self.path == '/':
             return 'Root'
         else:
             return os.path.basename(self.path)
     
-    def is_dir(self):
+    def is_dir(self) -> bool:
         """
         Check if the node is a directory.
-        @return: True if the node is a directory, False otherwise.
+        Returns:
+            bool: True if the node is a directory, False otherwise.
         """
         return os.path.isdir(self.path)
     
-    def get_brother(self):
+    def get_brother(self) -> list:
         """
         Get the siblings of the node (children of the parent directory).
-        @return: A list of Node objects representing the siblings, or an error message if the operation fails.
+        Returns:
+            list: A list of Node objects representing the siblings, or an error message if the operation fails.
         """
         try:
             parent = self.get_parent()
@@ -51,12 +58,14 @@ class Node:
         except:
             return ["Impossible to read the folder"]
     
-    def get_children(self, hidden : bool, sort : SortFile = SortFile.NAME):
+    def get_children(self, hidden : bool, sort : SortFile = SortFile.NAME) -> list:
         """
         Get the children of the node (files and directories within it).
-        @param hidden: If True, include hidden files (those starting with a dot).
-        @param sort: The sorting method to apply to the children (default is by name).
-        @return: A list of Node objects representing the children, or None if the node is not a directory.
+        Parameters:
+            hidden (bool): If True, include hidden files (those starting with a dot).
+            sort (enumeration.SortFile): The sorting method to apply to the children (default is by name).
+        Returns:
+            list: A list of Node objects representing the children, or None if the node is not a directory.
         """
         try:
             if hidden:
@@ -72,18 +81,20 @@ class Node:
         except:
             return self.get_brother()
         
-    def get_permissions(self):
+    def get_permissions(self) -> str:
         """
         Get the permissions of the node in octal format.
-        @return: A string representing the permissions in octal format (e.g., '755').
+        Returns:
+            str: A string representing the permissions in octal format (e.g., '755').
         """
         return oct(os.stat(self.path).st_mode)[-3:]
     
 
-    def get_content(self):
+    def get_content(self) -> list:
         """
         Get the content of the node if it is a file.
-        @return: A list of lines in the file, or an error message if the operation fails.
+        Returns:
+            list: A list of lines in the file, or an error message if the operation fails.
         """
         try:            
             with open(self.path, 'r') as f:
@@ -91,10 +102,11 @@ class Node:
         except:
             return ["Impossible to read the file"]
     
-    def get_parent(self):
+    def get_parent(self) -> 'Node':
         """
         Get the parent directory of the node.
-        @return: A Node object representing the parent directory.
+        Returns:
+            Node: A Node object representing the parent directory.
         """
         if self.path == '/':
             return Node('/')
@@ -104,14 +116,16 @@ class Node:
     def copy(self, node):
         """
         Copy the node to another node's path.
-        @param node: The destination Node object where the current node will be copied.
+        Parameters:
+            node (Node): The destination Node object where the current node will be copied.
         """
         os.system(f'cp -r {self.get_path()} {node.get_path()}')
 
     def move(self, node):
         """
         Move the node to another node's path.
-        @param node: The destination Node object where the current node will be moved.
+        Parameters:
+            node (Node): The destination Node object where the current node will be moved.
         """
         os.system(f'mv {self.get_path()} {node.get_path()}')
         
@@ -127,21 +141,24 @@ class Node:
     def create_file(self, name):
         """
         Create a new file with the specified name in the node's directory.
-        @param name: The name of the file to be created.
+        Parameters:
+            name (str): The name of the file to be created.
         """
         os.system(f'touch {os.path.join(self.get_path(), name)}')
         
     def create_dir(self, name):
         """
         Create a new directory with the specified name in the node's directory.
-        @param name: The name of the directory to be created.
+        Parameters:
+            name (str): The name of the directory to be created.
         """
         os.system(f'mkdir {os.path.join(self.get_path(), name)}')
         
     def rename(self, name):
         """
         Rename the node to the specified name.
-        @param name: The new name for the node.
+        Parameters:
+            name (str): The new name for the node.
         """
         os.system(f'mv {self.get_path()} {os.path.join(self.get_parent().get_path(), name)}')
     
@@ -149,13 +166,15 @@ class Node:
     def get_size(self):
         """
         Get the size of the node (file or directory).
-        @return: The size of the node in bytes, or 0 if the node does not exist.
+        Returns:
+            int: The size of the node in bytes, or 0 if the node does not exist.
         """
         return os.path.getsize(self.path)
     
     def get_date(self):
         """
         Get the last modification time of the node.
-        @return: The last modification time as a timestamp (seconds since epoch).
+        Returns:
+            float: The last modification time as a timestamp (seconds since epoch).
         """
         return os.path.getmtime(self.path)
